742e3fd0c9089e4239d2e5f309811e297154c51b
kwlucas <lucaswkyle@gmail.com>
2022-09-27 11:43:22 -0600

    created root prompt

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
 const cTable = require('console.table');
+
+const rootPrompt = [
+    {
+        type: 'list',
+        name: 'rootSelection',
+        message: "Select an action.",
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit'],
+    }
+]

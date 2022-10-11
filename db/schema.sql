33908448a434ce3d7a93f80d441cc59bde8b0f5a
kwlucas <lucaswkyle@gmail.com>
2022-10-11 10:32:35 -0600

    Adjustments to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -22,8 +22,9 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
+  manager_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL

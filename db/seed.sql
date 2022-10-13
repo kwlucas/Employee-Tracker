34d329bd2eade2c8985ef8f1752e0fe42fa021ec
kwlucas <lucaswkyle@gmail.com>
2022-10-13 13:26:22 -0600

    Seed department data. (Star Wars based :D )

--- db/seed.sql
+++ db/seed.sql
@@ -3,8 +3,9 @@ use employee_db;
 INSERT INTO department
     (name)
 VALUES
-    ('Department 1'),
-    ('Department 2');
+    ('Jedi'),
+    ('Military'),
+    ('Government');
 
 
 INSERT INTO role

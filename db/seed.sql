9f726029b70b68fc41c9def568cbeb4451648093
kwlucas <lucaswkyle@gmail.com>
2022-10-13 13:20:53 -0600

    Created placeholder emplyee seed

--- db/seed.sql
+++ db/seed.sql
@@ -13,3 +13,10 @@ VALUES
     ('Role 1', 100, 1),
     ('Role 2', 100, 1),
     ('Role 3', 100, 2);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('First', 'Last', 1, 1),
+    ('First', 'Last', 3, 1),
+    ('First', 'Last', 2, 2);

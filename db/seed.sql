bf1d3161c53e383d57b7c8478816e71c55d56089
kwlucas <lucaswkyle@gmail.com>
2022-10-13 13:17:41 -0600

    Created placeholder role seeds

--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,11 @@ INSERT INTO department
 VALUES
     ('Department 1'),
     ('Department 2');
+
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Role 1', 100, 1),
+    ('Role 2', 100, 1),
+    ('Role 3', 100, 2);

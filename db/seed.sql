8f6de0e7eb66c45682a14d8bfbd4d50d14f78659
kwlucas <lucaswkyle@gmail.com>
2022-11-23 16:14:05 -0700

    Changed seeds

--- db/seed.sql
+++ db/seed.sql
@@ -3,34 +3,36 @@ use employee_db;
 INSERT INTO department
     (name)
 VALUES
-    ('Jedi'),
-    ('Military'),
-    ('Government');
+    ('Operations'),
+    ('Finances'),
+    ('Engineering');
 
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Padawan', 30000, 1),
-    ('Knight', 60000, 1),
-    ('Master', 100000, 1),
-    ('Captain', 35000, 2),
-    ('Commander', 55000, 2),
-    ('Admiral', 65000, 2),
-    ('Representative', 50000, 3),
-    ('Senator', 75000, 3),
-    ('Chancellor', 125000, 3);
+    ('Assistant', 30000, 1),
+    ('Specialist', 60000, 1),
+    ('Team Lead', 100000, 1),
+    ('Intern', 35000, 2),
+    ('Analyst', 55000, 2),
+    ('Director', 65000, 2),
+    ('Apprentice', 50000, 3),
+    ('Technician', 75000, 3),
+    ('Manager', 125000, 3);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Sheev', 'Palpatine', 9, NULL),
-    ('Padme', 'Amidala', 8, 1),
-    ('Mon', 'Mothma', 8, 1),
-    ('Jar Jar', 'Binks', 7, 1),
-    ('Yoda', 'N/A', 3, NULL),
-    ('Anakin', 'Skywalker', 2, 2),
-    ('Ahsoka', 'Tano', 1, 2),
-    ('Obi-Wan', 'Kenobi', 3, 4),
-    ('Wilhuff', 'Tarkin', 4, 7),
-    ('Wullf', 'Yularen', 6, 7);
+    ('Clark', 'Kent', 3, NULL),
+    ('Bruce', 'Wayne', 6, NULL),
+    ('Diana', 'Prince', 2, 1),
+    ('John', 'Jones', 9, NULL),
+    ('Shayera', 'Hol', 2, 1),
+    ('Oliver', 'Queen', 5, 2),
+    ('Dinah', 'Lance', 5, 2),
+    ('Vic', 'Sage', 8, 4),
+    ('Will', 'Harper', 4, 6),
+    ('Dick', 'Grayson', 4, 2),
+    ('Zatanna', 'Zatara', 8, 4)
+
